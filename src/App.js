91c24947aa0686fb595c0a83c1f544b1fd0c816b
Yusong Shi <jackshi.sys@gmail.com>
2022-08-24 14:20:15 -0400

    result page added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Quiz from "./components/Quiz";
-import Results from "./components/Results";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/quiz" element={<Quiz />} />
-        <Route path="/results" element={<Results />} />
       </Routes>
     </>
   );
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const questions = [
   {
@@ -35,27 +36,56 @@ const questions = [
 
 function Quiz() {
   const [currentQes, setCurrentQes] = useState(0);
+  const [score, setScore] = useState(false);
+
+  const handleBtnClick = () => {
+    const nextQes = currentQes + 1;
+    if (nextQes < questions.length) {
+      setCurrentQes(nextQes);
+    } else {
+      setScore(true);
+    }
+  };
+
   return (
     <>
-      <div className="h-screen bg-bg-white relative flex min-h-screen flex-col items-center justify-center">
-        <div className="p-20 max-w-2xl text-white bg-bg-dark rounded-xl shadow-md flex flex-col items-start">
-          <div className="flex flex-col">
-            <p className="text-lg font-semibold text-gray-500 py-4">
-              Question 1/3
+      {score ? (
+        <div className="h-screen bg-bg-white relative flex min-h-screen flex-col items-center justify-center">
+          <div className="p-40 max-w-2xl text-white bg-bg-dark rounded-xl shadow-md flex flex-col items-center ">
+            <p className="py-10 font-medium text-2xl ">
+              Your score: 1 out of 3
             </p>
-            <p className="text-2xl font-medium">
-              {questions[currentQes].question}
-            </p>
-          </div>
-          <div className="flex flex-col items-start py-8">
-            {questions[currentQes].answerOpts.map((answeropt) => (
-              <button className="hover:bg-slate-300 hover:text-bg-dark text-white border px-4 py-2 my-2 rounded font-semibold">
-                {answeropt.answer}
+            <Link to="/">
+              <button className="bg-slate-300 text-bg-dark px-4 py-2 rounded font-semibold">
+                Start Over
               </button>
-            ))}
+            </Link>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="h-screen bg-bg-white relative flex min-h-screen flex-col items-center justify-center">
+          <div className="p-20 max-w-2xl text-white bg-bg-dark rounded-xl shadow-md flex flex-col items-start">
+            <div className="flex flex-col">
+              <p className="text-lg font-semibold text-gray-500 py-4">
+                Question {currentQes + 1}/3
+              </p>
+              <p className="text-2xl font-medium">
+                {questions[currentQes].question}
+              </p>
+            </div>
+            <div className="flex flex-col items-start py-8">
+              {questions[currentQes].answerOpts.map((answeropt) => (
+                <button
+                  className="hover:bg-slate-300 hover:text-bg-dark text-white border px-4 py-2 my-2 rounded font-semibold"
+                  onClick={handleBtnClick}
+                >
+                  {answeropt.answer}
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
