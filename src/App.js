6f8916628ec06041bc65fe97a106cdd0aa7fb5af
Yusong Shi <jackshi.sys@gmail.com>
2022-08-24 12:30:34 -0400

    path added, file structure added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Quiz from "./components/Quiz";
+import Results from "./components/Results";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </>
   );
 }
 
